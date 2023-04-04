03f93a7805f489ca368875340a2e1a8b36bf0c54
iputuwisuda <113968008+iputuwisuda@users.noreply.github.com>
2023-04-04 23:18:35 +0930

    updated views.py

--- group14_proj/group14_app/views.py
+++ group14_proj/group14_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    return render (request, 'group14_app/homepage.html') 
+
+def list (request):
+    return render (request, 'group14_app/list.html') 
+
+def details (request):
+    return render (request, 'group14_app/details.html') 
+
+def data_model (request):
+    return render (request, 'group14_app/data-model.html') 
