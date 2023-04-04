51a0ea05985e036b0e5404636abb598592a18405
iputuwisuda <113968008+iputuwisuda@users.noreply.github.com>
2023-04-04 23:16:40 +0930

    updated settings and urls

--- group14_proj/group14_proj/urls.py
+++ group14_proj/group14_proj/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from group14_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name ='homepage'),
+    path('home/', views.home, name ='homepage'),
+    path('list/', views.list, name ='list'),
+    path('details/', views.details, name ='details'),
+    path('data_model/', views.data_model, name ='data_model'),
 ]
