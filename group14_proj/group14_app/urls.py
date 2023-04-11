2073a084c12d95a015e0ffd4ed4782a422a3ead9
Emily11220 <129145254+Emily11220@users.noreply.github.com>
2023-04-11 23:06:46 +0930

    Update urls.py

--- group14_proj/group14_app/urls.py
+++ group14_proj/group14_app/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, re_path
+
+from group14_app import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name ='homepage'),
+    path('home/', views.home, name ='homepage'),
     path('list/', views.list, name ='list'),
-    path('details/', views.details, name ='details'),
+    path('details/<int:id>/', views.details, name = 'details'),
+    re_path(r'^details/(?P<id>[1-6]{1})/?$', views.details),
     path('data_model/', views.data_model, name ='data_model'),
-
-    #path('now/',views.now, name="now"),
 ]
