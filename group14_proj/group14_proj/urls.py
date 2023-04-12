56c9aacb9c0cb0bc0cc4a77ac9cffee372275184
amila <amila263@gmail.com>
2023-04-12 23:36:28 +1000

    Global URL configurations changed.

--- group14_proj/group14_proj/urls.py
+++ group14_proj/group14_proj/urls.py
@@ -15,16 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 from group14_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name ='homepage'),
-    path('home/', views.home, name ='homepage'),
-    path('list/', views.list, name ='list'),
-    path('details/<int:id>/', views.details, name = 'details'),
-    re_path(r'^details/(?P<id>[1-6]{1})/?$', views.details),
-    path('data_model/', views.data_model, name ='data_model'),
+    path('',include('group14_app.urls'))
 ]
