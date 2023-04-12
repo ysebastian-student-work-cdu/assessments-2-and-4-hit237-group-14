b9bdd4e7b079b044ae999b9758375aa052a27bbd
Emily11220 <129145254+Emily11220@users.noreply.github.com>
2023-04-12 23:27:03 +0930

    Update urls.py

--- group14_proj/group14_app/urls.py
+++ group14_proj/group14_app/urls.py
@@ -1,5 +1,21 @@
+"""
+URL configuration for group14_proj project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 
 from group14_app import views
 
@@ -8,7 +24,6 @@ urlpatterns = [
     path('', views.home, name ='homepage'),
     path('home/', views.home, name ='homepage'),
     path('list/', views.list, name ='list'),
-    path('details/<int:id>/', views.details, name = 'details'),
-    re_path(r'^details/(?P<id>[1-6]{1})/?$', views.details),
+    path('details/', views.details, name ='details'),
     path('data_model/', views.data_model, name ='data_model'),
 ]
