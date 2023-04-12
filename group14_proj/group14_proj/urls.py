987572f25f88749c896a577aa530142c62ce2c3a
Emily11220 <129145254+Emily11220@users.noreply.github.com>
2023-04-12 23:29:10 +0930

    Update urls.py

--- group14_proj/group14_proj/urls.py
+++ group14_proj/group14_proj/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 
 from group14_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('group14_app.urls'))
+    path('', views.home, name ='homepage'),
+    path('home/', views.home, name ='homepage'),
+    path('list/', views.list, name ='list'),
+    path('details/', views.details, name ='details'),
+    path('data_model/', views.data_model, name ='data_model'),
 ]
